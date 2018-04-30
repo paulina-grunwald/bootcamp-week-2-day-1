e1899506be1f5c87d5ddf73ff8b1b7deb3c3c390
Arien Kock <arien@positor.nl>
2018-04-30 14:38:28 +0200

    fix dependency on current date

--- src/printAges.js
+++ src/printAges.js
@@ -1,8 +1,13 @@
+let currentDate = Date.now
+module.exports.changeCurrentDate = function(newCurrentDate) {
+  currentDate = newCurrentDate
+}
+
 module.exports.printAges = function(users) {
   return users.map(user => {
     const { name, dateOfBirth } = user
     const birthday = Date.parse(dateOfBirth)
-    const ageDifMs = Date.now() - birthday
+    const ageDifMs = currentDate() - birthday
     const ageDate = new Date(ageDifMs)
     return `${name} is ${Math.abs(ageDate.getUTCFullYear() - 1970)} years old.`
   })
--- src/printAges.spec.js
+++ src/printAges.spec.js
@@ -1,6 +1,7 @@
 const assert = require("assert")
 const userData = require("./data/users")
-const { printAges } = require("./printAges")
+const { printAges, changeCurrentDate } = require("./printAges")
+changeCurrentDate(() => Date.parse("03-01-2018"))
 
 const [ava, james, danielle, darnell] = printAges(userData)
 
